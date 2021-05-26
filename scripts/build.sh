62324f1055ed38eeaaf962f3d8653b51e2d61b9c
Alexander Kromm <mmaulwurff@gmail.com>
2021-05-26 21:47:58 +0700

    workaround for new GZDoom text progress bar

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-set -e
-
-
 filename=build/status-widget-$(git describe --abbrev=0 --tags).pk3
 
 mkdir -p build
 rm -f   "$filename"
-zip -R0 "$filename" "*.md" "*.txt" "*.zs"
-gzdoom  "$filename" "$@"
+zip -R0 "$filename" "*.md" "*.txt" "*.zs" > /dev/null
+gzdoom  "$filename" "$@" > output 2>&1; cat output
